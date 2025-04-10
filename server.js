8ddbfc5603ab3a1c0993cdc1f741971836e464ed
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2025-04-10 19:03:20 +0530

    Deployment setup

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+const path = require('path');
+
 require('dotenv').config();
 const express = require('express');
+
 const connectDB = require('./config/db');
 const budgetRoutes = require('./routes/budgetRoutes');
 
 const cors = require('cors');
 
 const app = express();
+
 app.use(cors());
-connectDB();
 app.use(express.json());
 
+connectDB();
+
 app.use('/api/budget', budgetRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('/{*splat}', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
